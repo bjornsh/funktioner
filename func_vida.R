74ec28bd6eaff3c5fb35db17fecf14899b38acea
bjornsh <bsh@posteo.de>
2022-08-02 10:53:16 +0200

    Update func_vida.R

--- func_vida.R
+++ func_vida.R
@@ -25,7 +25,7 @@
 
 
 ####################
-### APR påstigande (rolling average per month for subsequent 12 months)
+### Dilax: APR påstigande (rolling average per month for subsequent 12 months)
 ### dvs 2021-07 = mean APR for 2021-07 to 2022-06  
 ####################
 
@@ -51,7 +51,7 @@ FROM
 # funk_apr_hpl_all_month(con = con)
 
 ####################
-### APR påstigande (rolling average for last 12 months)
+### Dilax: APR påstigande (rolling average for last 12 months)
 ### dvs 2021-07 = mean APR for 2021-07 to 2022-06  
 ####################
 
@@ -84,3 +84,133 @@ FROM
 }
 
 # funk_apr_hpl_latest_month(con = con)
+
+
+
+
+
+####################
+### Atries: hämta incheckningsdata för resekort och app   
+####################
+
+funk_incheck = function(con, datum_start, datum_stop){ # datum = "YYYY-MM-DD"
+  incheck = 
+    # append rader från kort och app query
+    rows_append(
+      ## Incheckning med kort
+      tbl(con, dbplyr::in_schema("AtriesExtract", "ACC_CHECKIN")) %>%
+        select(datum = CHECKINDATE,
+               checkin = CHECKINTIMESTAMP,
+               linje = LINIE,
+               hpl_id = EINSTIEG,
+               ticketid = CC_SERNO_HASHED) %>%
+        # filter relevant tidsperiod
+        filter(between(datum, paste0(datum_start, " ", "00:00:00.000"),
+                       paste0(datum_stop, " ", "00:00:00.000"))) %>%
+        select(-datum) %>%
+        # lägg till kolumn som identifierar origin
+        mutate(typ = "kort"),
+      
+      ## Incheckning med app
+      tbl(con, dbplyr::in_schema("BoBULExtract", "TicketEvent")) %>%
+        select(checkin = DateTime,
+               linje = LineNumber,
+               hpl_id = StopAreaId,
+               ticketid = TicketId) %>%
+        filter(between(checkin, paste0(datum_start, " ", "00:00:00.000"),
+                       paste0(datum_stop, " ", "00:00:00.000")))  %>%
+        mutate(typ = "app")
+      ) %>%
+    # fetch data
+    collect()
+  
+  return(incheck) }
+
+# incheck = funk_incheck(con, "2022-05-30", "2022-06-01")
+
+
+
+
+####################
+### Atries: antal personer som byter vid en hållplats   
+####################
+
+funk_byte = function(con, datum_start, datum_stop, max_sec_between_incheck){ 
+  # datum = "YYYY-MM-DD"
+  # max_sec_between_incheck, eg "3600" for one hour
+  
+  # hämta incheckningsdata
+  dat = funk_incheck(con, datum_start, datum_stop) %>% 
+    mutate(datum = substr(checkin, 1, 10))
+  
+  # total number of boarding per hpl and day (based on incheckningsdata NOT APR)
+  board = dat %>% 
+    group_by(datum, hpl_id) %>% 
+    summarise(boarding = n()) %>% 
+    ungroup() %>% 
+    filter(nchar(hpl_id) > 4)
+  
+  # antal dagar som ingår i urvalet
+  antal_dagar = length(unique(dat$datum))
+  
+  
+  ## remove all people with less than 2 påstigande per dygn
+  id_inklud = dat %>%
+    group_by(datum, ticketid) %>%
+    summarise(n = n()) %>%
+    ungroup() %>%
+    filter(n > 1) %>%
+    mutate(concat = paste(datum, ticketid)) %>% 
+    select(concat) %>% pull()
+  
+  dat1 = dat %>% 
+    mutate(concat = paste(datum, ticketid)) %>%
+    filter(concat %in% id_inklud)
+    
+  
+  ### calculate time difference between boardings with same ID on same day
+  x = dat1 %>% 
+    # convert to date-time
+    mutate(checkin = as.POSIXct(checkin)) %>%
+    # arrange in correct temporal order
+    group_by(concat) %>%
+    arrange(checkin, .by_group = TRUE) %>%
+    ungroup() %>% 
+    group_by(datum, checkin) %>% 
+    # calculate time diff between second & first, third & second etc
+    mutate(time_diff = checkin - lag(checkin)) %>% 
+    # remove all first boardings, ie start of journey
+    filter(!is.na(time_diff)) %>%
+    # remove all boardings later than 1 hour after previous
+    # as this is most likely a return journey and not a change to continue the journey
+    filter(time_diff <= max_sec_between_incheck)
+    
+    
+    # calculate number of people changing at a stop per date
+    byte = x %>% 
+      # number of changes per hpl and date
+      group_by(datum, hpl_id) %>% 
+      summarise(antal_byte = n()) %>%
+      ungroup() %>%
+      # remove unreasonable hpl_id
+      filter(nchar(hpl_id) > 4)
+    
+    ### join byte with total boarding (based on incheckning, NOT APR)
+    # hpl_id not present in df have 0 byte.
+    # antal_byte = 0 and andel_byte_av_boarding = 0 needs to be added
+    final = byte %>% 
+      left_join(., board, by = c("hpl_id", "datum")) %>%
+      # share of boarding that is a change between busses (no incheckning on trains)
+      # sum across days and then divide with number of days in sample
+      group_by(hpl_id) %>% 
+      summarise(antal_byte = sum(antal_byte) / antal_dagar,
+                boarding = sum(boarding) / antal_dagar) %>%
+      # andel byte av incheckning, dvs boarding enligt Atries
+      mutate(andel_byte_av_boarding = round(antal_byte / boarding, 2))
+    
+    return(final)
+}
+
+# byte = funk_byte(con, "2022-05-30", "2022-05-31", "3600")
+
+
