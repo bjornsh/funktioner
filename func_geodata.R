fcb485fb5836fb2673f11eeb3a98a6b2bc0c2e42
bjornsh <bsh@posteo.de>
2022-09-27 11:43:16 +0200

    scb center coordinate

---
+++ func_geodata.R
@@ -0,0 +1,53 @@
+
+
+### Skapa centerkoordinater för SCB rutnät data
+
+# SCB rut ID är en kod för nedre vänster hörn av rutan
+# Identifiera rutstorleken
+rutid_till_centercoord <- function(df, position){ # position av kolumnen med RutID
+  rutstorlek <- ifelse(max(substr(df[,position],11, 13)) == "900", 100, 
+                       ifelse(max(substr(df[,position],11, 13)) == "500", 500,
+                              ifelse(max(substr(df[,1],11, 13)) == "000", 1000, "-999")))
+  
+  # avstånd till centerkoordinaten
+  dist_to_center <- rutstorlek / 2
+  
+  # create grid center coordinates from grid ID (bottom left corner + grid diameter in x and y direction
+  output <- df %>%
+    as.data.frame() %>%
+    dplyr::select(rut = 1, last_col()) %>%  
+    mutate(x_center = as.numeric(substr(rut, 1, 6)) + dist_to_center,
+           y_center = as.numeric(substr(rut, 7, 13)) + dist_to_center) %>%
+    st_as_sf(
+      coords = c("x_center", "y_center"),
+      agr = "constant",
+      crs = 3006,        # assign SWEREF99 as CRS
+      stringsAsFactors = FALSE,
+      remove = TRUE
+    )
+  
+  return(output)
+}
+
+
+# Example
+
+# # create directory to store data
+# dir.create("data")
+# 
+# #### SCB befolkningsdata för 1km2 rutor (4.6 MB)
+# download.file("https://www.scb.se/contentassets/790b7863da264730b626e4289dcb15a5/grid1km_totpop_20181231.zip",
+#               destfile= "data/grid.zip")
+# 
+# unzip("data/grid.zip", exdir="data", overwrite=TRUE)
+#
+# filenames <- list.files(path="data",pattern="*shp")
+# 
+# scb = st_read(paste0("data/", filenames), 
+#               options = "ENCODING=WINDOWS-1252")
+#
+# rutid_till_centercoord(scb, 1) %>% mapview()
+
+
+
+
