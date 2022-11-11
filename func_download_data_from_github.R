4433edc1866a97891a43146c41f062a0a4de2f65
bjornsh <bsh@posteo.de>
2022-11-11 13:51:16 +0100

    new tätort and regso download function
    data not downloaded from Github but SCB hence requires webscraping to get latest url

--- func_download_data_from_github.R
+++ func_download_data_from_github.R
@@ -86,6 +86,100 @@ load_sf_deso <- function(){
 # deso <- load_sf_deso()
 
 
+### Download and load Regso shapefile from SCB
 
+load_sf_regso <- function(){
+  
+  # create temp directory
+  td = tempdir()
+  # create the placeholder file
+  tf = tempfile(tmpdir=td, fileext=".zip")
+  
+  # webscraping funktion from https://gist.github.com/paulrougieux/e1ee769577b40cd9ed9db7f75e9a2cc2
+  scraplinks <- function(url){
+    # Create an html document from the url
+    webpage <- xml2::read_html(url)
+    # Extract the URLs
+    url_ <- webpage %>%
+      rvest::html_nodes("a") %>%
+      rvest::html_attr("href")
+    # Extract the link text
+    link_ <- webpage %>%
+      rvest::html_nodes("a") %>%
+      rvest::html_text()
+    return(data_frame(link = link_, url = url_))
+  }
+  
+  # find url on download website
+  links = scraplinks("https://www.scb.se/vara-tjanster/oppna-data/oppna-geodata/regso/")
+  
+  # download into the placeholder file
+  download.file(paste0("https://www.scb.se/",
+                       links[links$link == "Geopackage (zip fil)",][[2]]), 
+                tf)
+  
+  # get name of the file ending with .shp
+  fname = unzip(tf, list=TRUE)$Name[grep("\\.gpkg$", unzip(tf, list=TRUE)$Name, ignore.case = T)]
+  
+  # unzip all files to the temporary directory
+  unzip(tf, exdir=td, overwrite=TRUE)
+  
+  # identify full path to extracted file ending with .shp
+  fpath = file.path(td, fname)
+  
+  # load the shapefile into R
+  sf::st_read(fpath, options = "ENCODING=WINDOWS-1252")
+}
+
+# regos <- load_sf_regso()
+
+
+### Download and load tätort shapefile from SCB
+# File size = 175MB
+  
+load_sf_tatort <- function(){
+    
+    # create temp directory
+    td = tempdir()
+    # create the placeholder file
+    tf = tempfile(tmpdir=td, fileext=".zip")
+    
+    # webscraping funktion from https://gist.github.com/paulrougieux/e1ee769577b40cd9ed9db7f75e9a2cc2
+    scraplinks <- function(url){
+      # Create an html document from the url
+      webpage <- xml2::read_html(url)
+      # Extract the URLs
+      url_ <- webpage %>%
+        rvest::html_nodes("a") %>%
+        rvest::html_attr("href")
+      # Extract the link text
+      link_ <- webpage %>%
+        rvest::html_nodes("a") %>%
+        rvest::html_text()
+      return(data_frame(link = link_, url = url_))
+    }
+    
+    # find url on download website
+    links = scraplinks("https://www.scb.se/vara-tjanster/oppna-data/oppna-geodata/tatorter/")
+    
+    # download into the placeholder file
+    download.file(paste0("https://www.scb.se/",
+                         links[links$link == "Geopackage (zip-fil)",][[2]]), 
+                  tf)
+    
+    # get name of the file ending with .shp
+    fname = unzip(tf, list=TRUE)$Name[grep("\\.gpkg$", unzip(tf, list=TRUE)$Name, ignore.case = T)]
+    
+    # unzip all files to the temporary directory
+    unzip(tf, exdir=td, overwrite=TRUE)
+    
+    # identify full path to extracted file ending with .shp
+    fpath = file.path(td, fname)
+    
+    # load the shapefile into R
+    sf::st_read(fpath, options = "ENCODING=WINDOWS-1252")
+  }
+
+# tatort <- load_sf_tatort()
 
 
